b914995f57717199b022aaec3218086e1a76d1c7
Ryan Hausen <ryan.hausen@gmail.com>
2018-10-23 11:29:39 -0700

    Added Abalone, LinearRegression, KNN-Regression
    Still need to add figures

--- src/regression.py
+++ src/regression.py
@@ -1,13 +1,40 @@
+import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error, r2_score
+
 import regressors as reg
 import regression_data as reg_data
 
 def main():
     for data in [reg_data.Abalone]:
+        metrics = []
         x, y = data.get_preprocessed_XY()
-        for model in [reg.Model_LinearRegression]:
+        x, y = x.values, y.values
+        kf = KFold(n_splits=10)
+
+        for model in [reg.Model_LinearRegression, reg.Model_KnnRegression]:
+
+            acc, r2 = [], []
             m = model(dict())
-            m.fit(x, y)
-    
+            for train_idx, test_idx in kf.split(x):
+                x_train, y_train = x[train_idx], y[train_idx]
+                x_test, y_test = x[test_idx], y[test_idx]
+
+                m.fit(x_train, y_train)
+
+                y_hat = m.predict(x_test)
+
+                acc.append(mean_squared_error(y_test, y_hat))
+                r2.append(r2_score(y_test, y_hat))
+
+            print('Report for {}'.format(m.get_name()))
+            print('MSE: {}'.format(np.mean(acc)))
+            print('R2: {}'.format(np.mean(r2)))
+
+
+
+
+
 if __name__=='__main__':
     main()
 
--- src/regressors.py
+++ src/regressors.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
 
 class Regressor:
     def __init__(self, params:dict):
@@ -21,16 +22,15 @@ class Regressor:
     def generate_plots(self, save_location='.'):
         raise NotImplementedError('generate_plots')
 
+    def name(self):
+        raise NotImplementedError('name')
+
 class Model_LinearRegression(Regressor):
     def __init__(self, params:dict):
         super().__init__(params)
-        self._model = None
-        self._fitted_model = None
 
     def setup(self, params:dict):
-        print('Setup')
         self._model = LinearRegression()
-        print(self._model)
 
     def fit(self, x:np.ndarray, y:np.ndarray):
         self._fitted_model = self._model.fit(x, y)
@@ -40,8 +40,35 @@ class Model_LinearRegression(Regressor):
 
     def get_learned_features(self) -> np.ndarray:
         if self._fitted_model:
-            return np.array([self._fitted_model.coef_, 
+            return np.array([self._fitted_model.coef_,
                              self._fitted_model.intercept_])
-    
+
     def generate_plots(self, save_location='.'):
         pass
+
+    def get_name(self):
+        return 'Linear Regression'
+
+
+class Model_KnnRegression(Regressor):
+    def __init__(self, params:dict):
+        super().__init__(params)
+
+    def setup(self, params:dict):
+        self._model = KNeighborsRegressor()
+
+    def fit(self, x:np.ndarray, y:np.ndarray):
+        self._fitted_model = self._model.fit(x, y)
+
+    def predict(self, x:np.ndarray) -> np.ndarray:
+        return self._fitted_model.predict(x)
+
+    def get_learned_features(self) -> np.ndarray:
+        if self._fitted_model:
+            return np.array([])
+
+    def generate_plots(self, save_location='.'):
+        pass
+
+    def get_name(self):
+        return 'K Nearest Neighbors'
