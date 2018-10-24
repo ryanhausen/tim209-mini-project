c2fc2d42cdce8a8fb863623d14292e6fe0c06c51
Ryan Hausen <ryan.hausen@gmail.com>
2018-10-24 15:17:43 -0700

    Candidate for initial submission (Udpated)
    Added:
    - Forest Fires Data
    - Wine Quality Data
    - SVM 
    - Decision Tree
    - Plots for MSE and R^2

--- src/regression.py
+++ src/regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -6,33 +7,67 @@ import regressors as reg
 import regression_data as reg_data
 
 def main():
-    for data in [reg_data.Abalone]:
-        metrics = []
+    models = [reg.Model_LinearRegression(dict()),
+              reg.Model_KnnRegression({'n':5}),
+              reg.Model_DecisionTree(dict()),
+              reg.Model_SupportVectorMachine(dict())]
+
+    for data in [reg_data.Abalone, reg_data.ForestFires, reg_data.WineQuality]:
         x, y = data.get_preprocessed_XY()
-        x, y = x.values, y.values
         kf = KFold(n_splits=10)
 
-        for model in [reg.Model_LinearRegression, reg.Model_KnnRegression]:
+        print('==============================')
+        print(data.get_name())
+        results = []
+        for model in models:
 
-            acc, r2 = [], []
-            m = model(dict())
+            mse, r2 = [], []
+
             for train_idx, test_idx in kf.split(x):
                 x_train, y_train = x[train_idx], y[train_idx]
                 x_test, y_test = x[test_idx], y[test_idx]
 
-                m.fit(x_train, y_train)
+                model.fit(x_train, y_train)
 
-                y_hat = m.predict(x_test)
+                y_hat = model.predict(x_test)
 
-                acc.append(mean_squared_error(y_test, y_hat))
-                r2.append(r2_score(y_test, y_hat))
+                mse.append(mean_squared_error(y_test, y_hat))
+                r2.append(r2_score(y_test, y_hat, multioutput=None))
 
-            print('Report for {}'.format(m.get_name()))
-            print('MSE: {}'.format(np.mean(acc)))
-            print('R2: {}'.format(np.mean(r2)))
+            name = model.get_name()
+            mse = np.mean(mse)
+            r2 = np.mean(r2)
 
+            results.append((name, {'mse':mse,'r2':r2}))
 
+            print('Report for {}'.format(name))
+            print('MSE: {}'.format(mse))
+            print('R2: {}'.format(r2))
+        print('==============================')
 
+        # https://matplotlib.org/gallery/statistics/barchart_demo.html
+        bar_width = 0.35
+        idx = np.arange(len(results))
+        fig, ax = plt.subplots(figsize=(10,5))
+        ax.bar(idx,
+               list(map(lambda a:a[1]['mse'], results)),
+               bar_width,
+               label='MSE')
+
+        ax.bar(idx+bar_width,
+               list(map(lambda a:a[1]['r2'], results)),
+               bar_width,
+               label='$R^2$')
+
+        ax.set_xlabel('Classifier')
+        ax.set_ylabel('Values')
+        ax.set_title('MSE and $R^2$ for Classifiers on {}'.format(data.get_name()))
+        ax.set_xticks(idx + bar_width / 2)
+        ax.set_xticklabels(list(map(lambda a: a[0], results)))
+        ax.legend()
+
+        fig.tight_layout()
+        plt.savefig('./{}_comparison.png'.format(data.get_name()))
 
 
 if __name__=='__main__':
--- src/regressors.py
+++ src/regressors.py
@@ -2,10 +2,13 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+
 
 class Regressor:
     def __init__(self, params:dict):
-        self.params
+        self.params = params
         self.setup(params)
 
     def setup(self, params:dict):
@@ -19,9 +22,6 @@ class Regressor:
 
     def get_learned_features(self):
         raise NotImplementedError('get_learned_features')
-
-    def generate_plots(self, save_location='.'):
-        raise NotImplementedError('generate_plots')
 
     def name(self):
         raise NotImplementedError('name')
@@ -44,9 +44,6 @@ class Model_LinearRegression(Regressor):
             return np.array([self._fitted_model.coef_,
                              self._fitted_model.intercept_])
 
-    def generate_plots(self, save_location='.'):
-        pass
-
     def get_name(self):
         return 'Linear Regression'
 
@@ -68,8 +65,46 @@ class Model_KnnRegression(Regressor):
         if self._fitted_model:
             return np.array([])
 
-    def generate_plots(self, save_location='.'):
-        pass
-
     def get_name(self):
         return 'K Nearest Neighbors: k={}'.format(self.params['n'])
+
+
+class Model_DecisionTree(Regressor):
+    def __init__(self, params:dict):
+        super().__init__(params)
+
+    def setup(self, params:dict):
+        self._model = DecisionTreeRegressor()
+
+    def fit(self, x:np.ndarray, y:np.ndarray):
+        self._fitted_model = self._model.fit(x, y)
+
+    def predict(self, x:np.ndarray) -> np.ndarray:
+        return self._fitted_model.predict(x)
+
+    def get_learned_features(self) -> np.ndarray:
+        if self._fitted_model:
+            return np.array([])
+
+    def get_name(self):
+        return 'Decision Tree'
+
+class Model_SupportVectorMachine(Regressor):
+    def __init__(self, params:dict):
+        super().__init__(params)
+
+    def setup(self, params:dict):
+        self._model = SVR()
+
+    def fit(self, x:np.ndarray, y:np.ndarray):
+        self._fitted_model = self._model.fit(x, y)
+
+    def predict(self, x:np.ndarray) -> np.ndarray:
+        return self._fitted_model.predict(x)
+
+    def get_learned_features(self) -> np.ndarray:
+        if self._fitted_model:
+            return np.array([])
+
+    def get_name(self):
+        return 'Support Vector Machine'
